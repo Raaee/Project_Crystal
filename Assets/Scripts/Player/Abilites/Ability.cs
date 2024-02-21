5d486689a409621500148e9227704d3dbf12696c
Ally <Roa.ayleen07@gmail.com>
2024-02-21 16:55:44 -0500

    Updated more constant variables

--- Assets/Scripts/Player/Abilites/Ability.cs
+++ Assets/Scripts/Player/Abilites/Ability.cs
@@ -10,5 +10,10 @@ abstract class Ability : MonoBehaviour
     public const int MAX_AOE_RADIUS = 0;
     public const int BASIC_DASH_RANGE = 0;
     public const int ATTACK_RANGE = 0;
+    public const int SPEED = 0;
+    public const int ABILITY_DURATION = 0;
+    public const int MANA_COST = 0;
+    public const float COOL_DOWN = 0;
+      
     
 }
