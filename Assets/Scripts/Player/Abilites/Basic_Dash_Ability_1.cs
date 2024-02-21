27e9ae5fadfc8a61ac85050ed037f046aab91c18
Ally <Roa.ayleen07@gmail.com>
2024-02-21 17:02:25 -0500

    removed basic attack and added basic dash as ability 1 and incremented the name of the other abilities

--- Assets/Scripts/Player/Abilites/Basic_Dash_Ability_1.cs
+++ Assets/Scripts/Player/Abilites/Basic_Dash_Ability_1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Melee_Ab : MonoBehaviour
+public class Basic_Dash : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/Player/Abilites/Melee Abilities/Melee_Ability_2.cs
+++ Assets/Scripts/Player/Abilites/Melee Abilities/Melee_Ability_2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Melee_Ability_2 : MonoBehaviour
+public class Melee_Ab : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
