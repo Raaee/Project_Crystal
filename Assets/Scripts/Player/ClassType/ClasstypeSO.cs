c4714472abce1bed78c8c29a4ddb0a5ed6a4f5fe
Isaacl78 <isaclugo2014@gmail.com>
2024-02-21 20:04:17 -0500

    Health script

--- Assets/Scripts/Player/ClassType/ClasstypeSO.cs
+++ Assets/Scripts/Player/ClassType/ClasstypeSO.cs
@@ -7,8 +7,7 @@ using UnityEngine;
 public class ClasstypeSO : ScriptableObject
 {
     public const int MAX_HEALTH = 100;
-    public const int MAX_MP = 100;
+    public const int MAX_MANA = 100;
     public const int MAX_SPEED = 0;
     public Sprite classSprite;
-
 }
