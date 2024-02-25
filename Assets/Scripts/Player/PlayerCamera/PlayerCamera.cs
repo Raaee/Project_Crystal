e1b2930ffe94ed8eeb2636b90bd0d6d2130d1177
Raaee <99899402+Raaee@users.noreply.github.com>
2024-02-24 19:19:13 -0500

    Update PlayerCamera.cs

--- Assets/Scripts/Player/PlayerCamera/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera/PlayerCamera.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerCamera : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(0f, 0f, -10f);
-    [SerializeField] private float smoothTime = 0.25f;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private float smoothTime = 0.3f;
     private Vector3 velocity = Vector3.zero;
 
     [SerializeField] private Transform camera; 
