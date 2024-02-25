2d3e20d9ecafe32302bc5e5c76aaa680a536e7cc
ally <roa.ayleen07@gmail.com>
2024-02-24 20:12:33 -0500

    Movement smoothness added, Beginnings of dash started need to work on OnDash.Invoke and add listener

--- Assets/Scripts/Player/Movement/characterMovement.cs
+++ Assets/Scripts/Player/Movement/characterMovement.cs
@@ -1,38 +1,74 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class characterMovement : MonoBehaviour
 {
-    [SerializeField] private float _speed;
-    private Rigidbody2D _rigidbody;
-    private Vector2 _movementInput;
-    private InputInitialize _playerInput;
-
+    [SerializeField] private float speed;
+    private Rigidbody2D rigidbody;
+    private Vector2 movementInput;
+    private InputInitialize playerInput;
+    private Vector2 smoothedMovementInput;
+    private Vector2 movementInputSmoothVelocity;
+    private float activeMoveSpeed;
+    private float dashSpeed;
+    public float dashLength = 0.5f, dashCoolDown = 1f;
+    private float dashCounter;
+    private float dashCoolCounter;
+    public event Action<float> OnDash;
+    private bool isDashing;
 
     private void Awake()
     {
-        _rigidbody ??= GetComponent<Rigidbody2D>();
-        _playerInput = GetComponent<InputInitialize>();
-       
+        rigidbody ??= GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<InputInitialize>();
+        activeMoveSpeed = speed;
+        
+
         // If there's no PlayerInput component attached, add one
-        if (_playerInput == null)
+        if (playerInput == null)
         {
-            _playerInput = gameObject.AddComponent<InputInitialize>();
+            playerInput = gameObject.AddComponent<InputInitialize>();
         }
     }
+
     private void FixedUpdate()
     {
+        smoothedMovementInput = Vector2.SmoothDamp(smoothedMovementInput, movementInput, ref movementInputSmoothVelocity, 0.1f);
         // Get the current movement input from the player
-        _movementInput = _playerInput.movement.ReadValue<Vector2>();
+        movementInput = playerInput.movement.ReadValue<Vector2>();
 
         // Apply the movement input to the rigidbody's velocity, scaled by the speed
-        _rigidbody.velocity = _movementInput * _speed;
+        rigidbody.velocity = smoothedMovementInput * activeMoveSpeed;
+        isDashing = playerInput.dash.ReadValue<float>() > 0.5f;
+
+        if (isDashing)
+        {
+            if (dashCoolCounter <= 0 && dashCounter <= 0)
+            {
+                activeMoveSpeed = dashSpeed * speed;
+                dashCounter = dashLength;
+            }
+        }
+
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+            if (dashCounter <= 0)
+            {
+                activeMoveSpeed = speed;
+                dashCoolCounter = dashCoolDown;
+            }
+        }
+
+        if (dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
+        }
     }
+}
 
     
-
-
-}
 
 
 
