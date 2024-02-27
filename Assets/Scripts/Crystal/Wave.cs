277737b9fcef0c4fb60feb5831f1ff592a272368
Victor Martinez <vicplusplus@proton.me>
2024-02-26 20:28:23 -0500

    Remove TimeDistribution in favor of capacity-based timing

--- Assets/Scripts/Crystal/Wave.cs
+++ Assets/Scripts/Crystal/Wave.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using System.Collections.Generic;
 
+/// <summary>
+/// Represents a wave of spawns.
+/// </summary>
 [System.Serializable]
 public class Wave
 {
@@ -8,11 +11,6 @@ public class Wave
     /// The maximum capacity of the wave.
     /// </summary>
     public int capacity;
-
-    /// <summary>
-    /// The time distribution mode for spawning the wave.
-    /// </summary>
-    public TimeDistribution timeDistribution;
 
     /// <summary>
     /// The list of spawns in the wave.
@@ -43,46 +41,12 @@ public class Wave
             Spawns[i].position = position;
             var random = ProbableObject.GetRandomProbableObject(probableSpawns, capacity - currentCapacity);
             Spawns[i].spawnObject = random.gameObject;
-
-            // Set time if time distributed by capacity
-            if (timeDistribution == TimeDistribution.ByCapacity)
-            {
-                Spawns[i].timeDelay = duration / capacity * currentCapacity;
-            }
+            Spawns[i].timeDelay = duration / capacity * currentCapacity;
 
             currentCapacity += random.capacity;
             i++;
         }
 
-        // Set spawn times if not distributed by capacity
 
-        // Random will always be a random time in the whole duration
-        if (timeDistribution == TimeDistribution.Random)
-        {
-            foreach (Spawn spawn in Spawns)
-            {
-                spawn.timeDelay = Random.Range(0, duration);
-            }
-        }
-        // Uniform will the spawns over time based on their index
-        int count = Spawns.Count;
-        if (timeDistribution == TimeDistribution.Uniform)
-        {
-            for (int j = 0; j < Spawns.Count; j++)
-            {
-                Spawns[j].timeDelay = duration / count * j;
-            }
-        }
-    }
-
-    /// <summary>
-    /// The time distribution modes for spawning the wave.
-    /// </summary>
-    [System.Serializable]
-    public enum TimeDistribution
-    {
-        Random,
-        ByCapacity,
-        Uniform
     }
 }
