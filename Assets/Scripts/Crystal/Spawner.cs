28bdd9dc1b1924cc74bf53b47dab985c352ee18e
Victor Martinez <vicplusplus@proton.me>
2024-02-26 20:16:30 -0500

    Spawning logic and documentation

--- Assets/Scripts/Crystal/Spawner.cs
+++ Assets/Scripts/Crystal/Spawner.cs
@@ -19,6 +19,12 @@ public class Spawner : MonoBehaviour
     public float cooldownTime;
 
     /// <summary>
+    /// The parent transform for the spawned objects.
+    /// </summary>
+    /// <remarks>
+    public Transform spawnParent;
+
+    /// <summary>
     /// The list of objects that can be spawned.
     /// </summary>
     public List<ProbableObject> spawnObjects;
@@ -27,6 +33,12 @@ public class Spawner : MonoBehaviour
     /// The list of waves to be generated.
     /// </summary>
     public List<Wave> waves;
+
+    public List<Transform> spawnedObjects;
+
+    public State state;
+
+    public float time;
 
     /// <summary>
     /// Spawns objects for each wave based on the specified radius, duration, and spawn objects.
@@ -42,5 +54,94 @@ public class Spawner : MonoBehaviour
         {
             wave.GenerateSpawns(radius, duration, spawnObjects);
         }
+    }
+
+    public Transform Spawn(Spawn spawn)
+    {
+        var obj = Instantiate(spawn.spawnObject, spawn.position, Quaternion.identity, spawnParent);
+
+        // TODO: Possibly modify enemy script later to add spawnedBy reference
+
+        return obj.transform;
+    }
+
+    /// <summary>
+    /// Update loop for the spawner.
+    /// </summary>
+    /// <remarks>
+    /// This method updates the state of the spawner based on the current state and time. It uses a state machine pattern to
+    /// handle the spawning, cooldown, and completion of waves.
+    /// </remarks>
+    public void Update()
+    {
+        switch (state)
+        {
+            case State.Idle:
+                break;
+            case State.Spawning:
+                time += Time.deltaTime;
+                if (waves.Count == 0)
+                {
+                    state = State.Complete;
+                    break;
+                }
+                else
+                {
+                    while (waves[0].Spawns.Count != 0 && waves[0].Spawns[0].timeDelay <= time)
+                    {
+                        var spawn = waves[0].Spawns[0];
+                        spawnedObjects.Add(Spawn(spawn));
+                        waves[0].Spawns.RemoveAt(0);
+                    }
+
+                    // Handle transition to cooldown state if there are no more objects to spawn and all transforms in the spawned list are destroyed
+                    // Not counting the children of the spawnParent, as they can possibly be moved to another parent, and that feels like an unncessary constraint
+                    if (waves[0].Spawns.Count == 0 && spawnedObjects.TrueForAll(obj => obj == null))
+                    {
+                        waves.RemoveAt(0);
+                        spawnedObjects.Clear(); // Clear destroyed objects from the list
+                        state = State.Cooldown;
+                        time = 0;
+                        break;
+                    }
+                }
+                break;
+            case State.Cooldown:
+                time += Time.deltaTime;
+                if (time >= cooldownTime)
+                {
+                    state = State.Spawning;
+                    time = 0;
+                }
+                break;
+            case State.Complete:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Possible states for the spawner.
+    /// <list type="bullet">
+    /// <item>
+    /// <term>Idle</term> <description>The spawner has not yet activated.</description>
+    /// </item>
+    /// <item>
+    /// <term>Spawning</term> <description>The spawner is currently spawning a wave of objects, or the currently spawned wave is not yet cleared.</description>
+    /// </item>
+    /// <item>
+    /// <term>Cooldown</term> <description>The spawner is currently in cooldown between waves. Only occurs if there are waves remaining and the spawned list is empty.</description>
+    /// </item>
+    /// <item>
+    /// <term>Complete</term> <description>The spawner has completed all waves and is no longer active.</description>
+    /// </item>
+    /// </list>
+    /// </summary>
+    [System.Serializable]
+    public enum State
+    {
+        Idle,
+        Spawning,
+        Cooldown,
+        Complete
     }
 }
