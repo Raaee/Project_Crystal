50d1f53f08a3b4e1f5d5851c42446c087a210539
Raaee <99899402+Raaee@users.noreply.github.com>
2024-02-27 15:47:05 -0500

    playerMovement update and scene

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -3,38 +3,30 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
+
+    private InputControls playerInput;
+
     [SerializeField] private float moveSpeed = 10;
     private Rigidbody2D rigidbody;
     private Vector2 movementInput;
-    private InputControls playerInput;
     private Vector2 smoothedMovementInput;
     private Vector2 movementInputSmoothVelocity; 
     
-    private void Awake()
-    {
-        rigidbody ??= GetComponent<Rigidbody2D>();
+    private void Awake()    {
+        rigidbody = GetComponent<Rigidbody2D>();
         playerInput = GetComponent<InputControls>();
-        
-        gameObject.GetComponent<TrailRenderer>().enabled = false;
-        // If there's no PlayerInput component attached, add one
-        if (playerInput == null)
-        {
-            playerInput = gameObject.AddComponent<InputControls>();
-        }
     }
-    private void FixedUpdate()
-    {
-        smoothedMovementInput = Vector2.SmoothDamp(smoothedMovementInput, movementInput, ref movementInputSmoothVelocity, 0.1f);
+    private void FixedUpdate()  {
         // Get the current movement input from the player
         movementInput = playerInput.movement.ReadValue<Vector2>();
+        smoothedMovementInput = Vector2.SmoothDamp(smoothedMovementInput, movementInput, ref movementInputSmoothVelocity, 0.1f);
 
         // Apply the movement input to the rigidbody's velocity, scaled by the speed
         rigidbody.velocity = smoothedMovementInput * moveSpeed;
+
     }
-    public Vector2 GetMovementInput()
-    {
+    public Vector2 GetMovementInput()   {
         return movementInput;
     }
 }
