f9938fb6f91d0d4d13f79ae5876b3ba0f7dd07f8
lucianadicampli03 <119644042+lucianadicampli03@users.noreply.github.com>
2024-02-28 15:02:51 -0500

    Testing script
    Tested Berserk and mana drop in hierarchy

--- Assets/Scripts/Enemy/DropLogic/BezerkerDrop.cs
+++ Assets/Scripts/Enemy/DropLogic/BezerkerDrop.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BezerkerDrop : DropData
+public class BezerkerCubeDrop : DropData
 
 {
     public float berserkerDamageIncrease;
     public float berserkerDamageTime;
+
+     private void Start()
+    {
+    OnDropInteract();
+    }
   public override void OnDropInteract()
    {
-     Debug.Log("Increasing damage by" + berserkerDamageIncrease + "for" + berserkerDamageTime + "seconds.");
+     Debug.Log("Increasing damage by " + berserkerDamageIncrease + " for " + berserkerDamageTime + " seconds.");
 
    }    
 
--- Assets/Scripts/Enemy/DropLogic/ManaDrop.cs
+++ Assets/Scripts/Enemy/DropLogic/ManaDrop.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class ManaDrop : DropData
 {
     public int manaAmount;
+     private void Start()
+    {
+    OnDropInteract();
+    }
       public override void OnDropInteract()
    {
  Debug.Log("Giving the player manaAmount" + manaAmount);
