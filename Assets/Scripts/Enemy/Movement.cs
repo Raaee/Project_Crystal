4e8d18f73689ab0007cab1cfb0172fd9014941b8
metrobobin <68521312+metrobobin@users.noreply.github.com>
2024-03-04 20:48:26 -0500

    clean up

--- Assets/Scripts/Enemy/Movement.cs
+++ Assets/Scripts/Enemy/Movement.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    private GameObject target1;
+    [SerializeField] private float baseSpeed;
+    [SerializeField] private float maxSpeed;
+    private float curSpeed;
+    private GameObject player;
     private Transform spawnedBy;
     private bool isAggroed;
     [SerializeField] private float aggroTime = 5f;
@@ -15,6 +17,7 @@ public class Movement : MonoBehaviour
     public void Start()
     {
         SetInitialTarget();
+        curSpeed = baseSpeed;
     }
 
     void SetInitialTarget()
@@ -45,26 +48,31 @@ public class Movement : MonoBehaviour
     }
 
 
-    public void ChangeSpeed(float amount)
+    public void SetSpeed(float amount)
     {
-        this.speed += amount;
+        this.curSpeed = amount;
+        //After duration of speed amp/nerf is over, Ienumerators
+        this.curSpeed = baseSpeed;
     }
 
     void MoveTowardsTarget(Transform target)
     {
         Vector3 direction = (target.position - transform.position).normalized;
-        transform.Translate(-direction * speed * Time.deltaTime);
+        transform.Translate(-direction * curSpeed * Time.deltaTime);
     }
-    public void ReceiveDamage()
+    [com.cyborgAssets.inspectorButtonPro.ProButton]
+    public void Aggro()
     {
         isAggroed = true;
-        currTarget = target1.transform;
+        currTarget = player.transform;
         aggroTimer = aggroTime;
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        ReceiveDamage(); //Set up when collision between attack/ability hits enemy
+        Aggro(); //Set up when collision between attack/ability hits enemy
         //Also here can set up when to stop moving (if hits body of crystal or player)
+        // SetSpeed(0) until out of range
+        // Or disable MoveTowardsTarget, set target to null?
     }
 
 }
