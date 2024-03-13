5b2341d9bb9d397c341be00c73351de51c06fd1f
Raaee <99899402+Raaee@users.noreply.github.com>
2024-03-13 03:07:42 -0400

    Revert "Revert "chest drop loot table""
    This reverts commit 39e20374ad9b9dc0a365857778b5ee80c659378e.

--- Assets/Scripts/Enemy/DropLogic/ListOfDropData.cs
+++ Assets/Scripts/Enemy/DropLogic/ListOfDropData.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class ListOfDropData : MonoBehaviour
 {
-   public HealthDrop healthDrop;
+    public HealthDrop healthDrop;
 
-   public ManaDrop manaDrop;
+    public ManaDrop manaDrop;
 
-   public BezerkerCubeDrop bezerkerCubeDrop;
-    // 
+    public BezerkerCubeDrop bezerkerCubeDrop;
+   
 }
