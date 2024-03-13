32fb7233eb9900157022d3b37f65f31019173628
Raaee <varangurenv@gmail.com>
2024-03-13 12:15:29 -0400

    organization

--- Assets/Scripts/Player/PlayerStats/ClasstypeSO.cs
+++ Assets/Scripts/Player/PlayerStats/ClasstypeSO.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 
 public class ClasstypeSO : ScriptableObject
 {
-    public const int MAX_HEALTH = 100;
-    public const int MAX_MANA = 100;
-    public const int MAX_SPEED = 0;
+    public int MAX_HEALTH = 100;
+    public int MAX_MANA = 100;
+    public int MAX_SPEED = 10;
     public Sprite classSprite;
 }
