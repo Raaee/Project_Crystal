616d971b5d3a16c25318678722f6e77df89be63a
dieg0rtega <160441988+dieg0rtega@users.noreply.github.com>
2024-03-18 16:38:08 -0400

    Finished summary for EnemyAI and Movement

--- Assets/Scripts/Enemy/EnemyAI/AI/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI/AI/EnemyAI.cs
@@ -6,21 +6,13 @@ using UnityEngine;
   <summary> 
   - Explain what the script is doing
 
-    1. Start():
-    2. SetInitialTarget():
-    3. Update():
-    4. InDanger():
-    5. InRange():
-    6. OnCollisionEnter2D():
-    7. enum EnemyAIType:
-    8. enum EnemyAttackType:
-    9. enum EnemyState:
+        EnemyAI derives from the abstract class Movement and begins by calling the SetInitialTarget() method. Depending on the enemyCurrentState,
+    the Enemy will change from various states within the enum EnemyState which affects its movement.
 
   - Purpose for the script
 
         This script sets up the complex AI for Enemy movement, whether the Enemy is targeting the Crystal or Player
-    and how it moves in relation to those targets in specific situations:
-    - 
+    and how it moves in relation to those targets in specific situations.
 
   - Code change suggestions
 
@@ -30,27 +22,39 @@ using UnityEngine;
 
 public class EnemyAI : Movement
 {
+    // Serialized Target Variables
     [SerializeField] private Transform player;
+    [SerializeField] private Transform crystalObject;
+
+    // Serialized Time Start Variables
     [SerializeField] private float aggroTime = 2f;
     [SerializeField] private float avoidTime = 1f;
+
+    //Serialized Enum Variables
     [SerializeField] private EnemyAIType enemyAIType;
     [SerializeField] private EnemyAttackType enemyAttackType;
     [SerializeField] private EnemyState enemyCurrentState;
+    
+    // Serialized Misc Variables
     [SerializeField] private bool inDanger;
     [SerializeField] private float attackRange;
-    [SerializeField] private Transform crystalObject;
+
+    // 
     private Transform currTarget;
     private float currentAggroTimer;
     private bool isAggroTimerActive = false;
     private float currentAvoidTimer;
     private bool isAvoidTimerActive = false;
 
+    // Start() calls SetInitialTarget()
     private void Start()
     {
 
         SetInitialTarget();
     }
 
+    // SetInitialTarget(), if crystalObject is not null, sets currTarget to crystalObject and enemyCurrentState to MOVETOWARDSCRYSTAL
+    // If crystalObject is null, enemyCurrentState is set to MOVETOWARDSPLAYER
     public void SetInitialTarget()
     {
         if (!crystalObject)
@@ -63,6 +67,10 @@ public class EnemyAI : Movement
         enemyCurrentState = EnemyState.MOVETOWARDSCRYSTAL;
     }
 
+    // Update() calls two if statements and a single switch-case statement
+    // The if statements check if isAvoidTimerActive/isAggroTimerActive are true and start a timer
+    // enemyCurrentState is set to MOVETOWARDSCRYSTAL when timer reaches 0
+    // The switch-case statement is repeatedly called to change enemyCurrentState to any of the states within the EnemyState enum it is set to
     private void Update()
     { 
         if (isAvoidTimerActive)
@@ -116,9 +124,9 @@ public class EnemyAI : Movement
         }
     }
 
-    //[com.cyborgAssets.inspectorButtonPro.ProButton]
-
-    // INDANGER METHOD, similar to aggro. will be used for hivemind/avoidant AI.
+    // InDanger() sets inDanger to true and checks if enemyAIType is AVOIDANT or HIVEMIND
+    // Currently a useless method, but it is supposed to cause AVOIDANT enemies to flee when attacked by the Player
+    // HIVEMIND enemies would not only aggro, but call nearby Enemies to fight alongside them against the Player
     public void InDanger()
     {
         inDanger = true;
@@ -131,8 +139,11 @@ public class EnemyAI : Movement
         }
     }
 
-    // INRANGE METHOD
-    /*public void InRange()
+    // InRange() checks if the Enemy's distance from a target surpasses attackRange
+    // If attackRange is surpassed, the Enemy switches to ATTACKINGPLAYER/CRYSTAL if it was previously MOVETOWARDSPLAYER/CRYSTAL respectively
+    // If attackRange is not surpassed and the Enemy is in ATTACKINGPLAYER/CRYSTAL, then it reverts to MOVINGTOWARDSPLAYER/CRYSTAL respectively
+    /*
+    public void InRange()
     {
         if (attackRange >= Vector3.Distance(transform.position, currTarget.transform.position))
         {
@@ -156,23 +167,25 @@ public class EnemyAI : Movement
                 enemyCurrentState = EnemyState.MOVETOWARDSCRYSTAL;
             }
         }
-    }*/
+    }
+    */
 
+    // OnCollisionEnter2D() checks if enemyCurrentState is MOVETOWARDSCRYSTAL
+    // If true, it sets that state to MOVETOWARDSPLAYERS and calls InDanger()
     void OnCollisionEnter2D(Collision2D collision)
     {
-
-        //if enemy curr state is MOVETOWARDSCRYSTAL
-        //  enemy curr state = MOVETOWARDSPLAYER
         if (enemyCurrentState == EnemyState.MOVETOWARDSCRYSTAL) {
             enemyCurrentState = EnemyState.MOVETOWARDSPLAYER;
             InDanger();
         }
-
-        
     }
 
 }
 
+// EnemyAIType can be set to AVOIDANT, HIVEMIND, or IDLE
+// AVOIDANTs will contain behavior where they run from the Player
+// HIVEMINDs will contain behavior wherein they call the for the aid of nearby Enemies
+// IDLEs behave normally
 public enum EnemyAIType
 {
     AVOIDANT,
@@ -180,12 +193,19 @@ public enum EnemyAIType
     NONE
 }
 
+// EnemyAttackType can be set to MELEE or RANGE
+// Movement-wise, this enum is needed to determine the attackRange value
 public enum EnemyAttackType
 {
     MELEE,
     RANGED
 }
 
+// EnemyState can be set to MOVETOWARDSPLAYER, ATTACKINGPLAYER, MOVETOWARDSCRYSTAL, ATTACKINGCRYSTAL, MOVEAWAY, or IDLE
+// MOVETOWARDSPLAYER and MOVETOWARDSCRYSTAL will move the Enemy to the Player or Crystal respectively
+// ATTACKINGPLAYER and ATTACKINGCRYSTAL will stop the Enemy and activate some sort of attack method against the Player or Crystal respectively
+// MOVEAWAY will be called to move the Enemy away from the Player
+// IDLE will make the Enemy stationary
 public enum EnemyState
 {
     MOVETOWARDSPLAYER,
--- Assets/Scripts/Enemy/EnemyAI/AI/Movement.cs
+++ Assets/Scripts/Enemy/EnemyAI/AI/Movement.cs
@@ -1,49 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 /* 
   <summary> 
   - Explain what the script is doing
 
-    1. Start(): the script sets the curSpeed of the GameObject to the baseSpeed (baseSpeed is set on initialization).
-    2. SetSpeed(): takes the argument of a float called 'amount' which curSpeed is set to and, after a duration,
-       sets curSpeed back to baseSpeed.
-    3. MoveTowardsTarget() and MoveAwayFromTarget(): take the argument of a Transfrom called 'target' (should come
-       from an external GameObject). A Vector3 called 'direction' calculates where the target is relative to Enemy.
-       Enemy then Translates based on that direction (multiplied by curSpeed and Time.deltaTime). Positive direction
-       moves towards target while negative moves away.
-    4. OnCollisionEnter2D(): curSpeed is set to 0 on collision.
-    5. OnCollisionExit2D(): curSpeed reverts back to baseSpeed exiting the collision.
+        Movement is an abstract class which sets up the Enemy's speed and methods of movement. SetSpeed() changes the curSpeed variable.
+    MoveTowardsTarget() and MoveAwayFromTarget() take in a target's transform to move the Enemy to or away from that target. The collision
+    methods set the curSpeed to zero and revert it to baseSpeed on exit.
 
   - Purpose for the script
 
-        This script sets up the basic movement elements for the Enemy GameObject, keeping track of a certain Enemy's
-    base speed and what speed it has at any current moment during gameplay:
-    - SetSpeed() augments current speed in situations where the enemy is slowed down or gains a boost.
-    - MoveTowardsTarget() and MoveAwayFromTarget() move the Enemy to or from another GameObject's position.
-    - The Collision methods stop the Enemy from moving and shuffling around its target on collision.
+        This script sets up the basic movement elements for the Enemy GameObject, keeping track of an Enemy's base speed and what speed it has
+    at any current moment during gameplay. The script also allows for the speed to be set to a specific value and for the Enemy to move to or
+    away from another GameObject's position, notably the Crystal or Player. Collision methods stop the Enemy from shuffling when in contact with
+    another GameObject.
 
   - Code change suggestions
 
     - The maxSpeed variable may be unnecessary.
     _ MoveTowardsTarget() and MoveAwayFromTarget() can be turned into one method which takes in a bool that
       determines if direction is positive or negative.
-    - Purpose for collision code should be moved to EnemyAI since Ranged Enemies would need different logic.
+    - Purpose for collision code is already partially done on EnemyAI, need to be clear on what needs to be done for eachs
   </summary>
 */
 
 public abstract class Movement : MonoBehaviour
 {
+    // Serialized Speed Variables
     [SerializeField] protected float baseSpeed;
     [SerializeField] protected float maxSpeed;
     [SerializeField] protected float curSpeed;
-   
+
+    // Start() sets curSpeed to baseSpeed (baseSpeed should be set on initialization of Enemy)
     private void Start()
     {
         curSpeed = baseSpeed;
     }
 
+    // SetSpeed() takes a float argument, 'amount', which curSpeed is set to and then sets curSpeed back to baseSpeeda after a duration.
     public void SetSpeed(float amount)
     {
         this.curSpeed = amount;
@@ -51,23 +49,28 @@ public abstract class Movement : MonoBehaviour
         this.curSpeed = baseSpeed;
     }
 
+    // MoveTowardsTarget() takes a Transform argument, 'target' (this is set on the EnemyAI script).
+    // A Vector3 called 'direction' calculates where the target is relative to Enemy and then Enemy Translates with that Vector3.
     public void MoveTowardsTarget(Transform target)
     {
         Vector3 direction = (target.position - transform.position).normalized;
         transform.Translate(direction * curSpeed * Time.deltaTime);
     }
 
+    // MoveAwayFromTarget() works the same as MoveTowardsTarget(), just with a negative direction Vector3.
     public void MoveAwayFromTarget(Transform target)
     {
         Vector3 direction = (target.position - transform.position).normalized;
         transform.Translate(-direction * curSpeed * Time.deltaTime);
     }
-    
+
+    // OnCollisionEnter2D() sets curSpeed to 0 on collision
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // Speed is set to 0
     }
 
+    // OnCollisionExit2D() reverts curSpeed back to baseSpeed on exiting a collision
     private void OnCollisionExit2D(Collision2D collision)
     {
         // Speed returns
