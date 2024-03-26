5feb45570884624ad2e74291d49ad33dbf8fd186
Isaacl78 <isaclugo2014@gmail.com>
2024-03-25 21:18:13 -0400

    Remove health on water collision(need to work more)

--- Assets/Scripts/Player/PlayerStats/PlayerHealthPoints.cs
+++ Assets/Scripts/Player/PlayerStats/PlayerHealthPoints.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerHealthPoints : HealthPoints
 {
     InputControls input;
+    private float countdown = 100;
     public override void Start() {
         base.Start();
         input = GetComponent<InputControls>();
@@ -21,9 +22,14 @@ public class PlayerHealthPoints : HealthPoints
         //input.OnEnable();
     }
 
-    public void OnTriggerEnter2D(Collider2D x) {
-        if (x.gameObject.tag == "Water") { 
-            Debug.Log("Remove Health");
+    public void OnTriggerEnter2D(Collider2D x)
+    {
+        
+        if (x.gameObject.tag == "Water")
+        {
+            countdown -= Time.deltaTime;
+            if (countdown <= 0)
+                RemoveHealth(1);
         }
-     }
+    }
 }
