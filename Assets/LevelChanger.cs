9dec6bac74c1556699995d3e8562d79a41d67ed2
memosama <carlosthegamingwizard@gmail.com>
2024-04-05 16:21:21 -0400

    update fade

--- Assets/LevelChanger.cs
+++ Assets/LevelChanger.cs
@@ -7,17 +7,10 @@ public class LevelChanger : MonoBehaviour
     public Animator animator;
 
     private int levelToLoad;
-
-
-
-
-    void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
-        {
-            FadeToLevel(1);
-        }
-
+    
+    public void StartFade ()
+    {   
+        FadeToLevel(1);
     }
 
     public void FadeToLevel (int levelIndex)
