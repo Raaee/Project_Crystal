ae05b4f247f7540f672548a6dfea1073cfbce0b2
ally <roa.ayleen07@gmail.com>
2024-04-08 22:34:11 -0400

    complete death panel
    Co-Authored-By: Raeus Aranguren-Viegas <99899402+Raaee@users.noreply.github.com>

--- Assets/Scripts/UI/DeathPanel.cs
+++ Assets/Scripts/UI/DeathPanel.cs
@@ -9,25 +9,38 @@ public class DeathPanelTimer : MonoBehaviour
     public TextMeshProUGUI countdownText;
     private float countdown;
     private bool isCountdownFinished = false;
+    private float respawnDelayTime = 1.5f;
+    [SerializeField] private Canvas deathPanelCanvas;
+    [SerializeField] private TextMeshProUGUI respawnText;
+    
 
     // Reference to the CanvasGroup for fading
     private CanvasGroup canvasGroup;
 
-    void Start()
+    private void Start()
     {
+        deathPanelCanvas.worldCamera = FindFirstObjectByType<Camera>();
         countdown = countdownDuration;
-        StartTimer();
+        respawnText.color = Color.red;
+        respawnText.text = "You Died...";
+        countdownText.gameObject.SetActive(false);
+        StartCoroutine(DelayRespawn());
 
         // Get the CanvasGroup component from the DeathPanel
         canvasGroup = GetComponent<CanvasGroup>();
-        if (canvasGroup == null)
-        {
-            Debug.LogError("CanvasGroup component not found.");
-        }
     }
 
+    public IEnumerator DelayRespawn()
+    {
+        
+        yield return new WaitForSeconds(respawnDelayTime);
+        respawnText.color = Color.white;
+        respawnText.text = "Respawn in";
+        StartTimer();
+    }
     public void StartTimer()
     {
+        countdownText.gameObject.SetActive(true);
         InvokeRepeating("UpdateTimer", 0f, 1f);
     }
 
@@ -35,24 +48,25 @@ public class DeathPanelTimer : MonoBehaviour
     {
         if (!isCountdownFinished)
         {
-            StartCoroutine(FadeIn());
+            //StartCoroutine(FadeIn());
             countdown -= 1f;
             countdownText.text = countdown.ToString();
             if (countdown <= 0)
             {
                 // Fade out the death panel
-                StartCoroutine(FadeOut());
+                //StartCoroutine(FadeOut());
                 isCountdownFinished=true;
                 // Respawn player or perform any other relevant action
                 // For example: GameManager.Instance.RespawnPlayer();
                 // You might want to disable the death panel here too
                 PlayerManager.Instance.Respawn();
                 countdownText.gameObject.SetActive(false);
+                Destroy(deathPanelCanvas.gameObject);
             }
         }
     }
 
-    IEnumerator FadeOut()
+ /*   IEnumerator FadeOut()
     {
         if (canvasGroup != null)
         {
@@ -84,5 +98,5 @@ public class DeathPanelTimer : MonoBehaviour
         {
             Debug.LogError("CanvasGroup component not found.");
         }
-    }
+    }*/
 }
