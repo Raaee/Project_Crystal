ec2c6f8688cc454e5a96d23b6bbccb09442239a6
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2024-04-12 09:03:11 -0400

    main menu sliders

--- Assets/Scripts/Audio/MainMenuAudio.cs
+++ Assets/Scripts/Audio/MainMenuAudio.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenuAudio : MonoBehaviour
 {
@@ -10,11 +11,21 @@ public class MainMenuAudio : MonoBehaviour
     [SerializeField] private AudioClip uiAcceptClip;
     [SerializeField] private AudioClip uiBackClip;
     [SerializeField] private AudioClip uiHoverClip;
+    [SerializeField] private AudioClip startMainMenuClip;
 
     [Header("Audio Mixer Slider")]
     [SerializeField] private AudioMixer mainAudioMixer;
-  
-  
+    [SerializeField] private Slider musicSlidre;
+
+    [SerializeField] private Slider sfxSlidre;
+
+
+    private void Start()
+    {
+        musicSlidre.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlidre.onValueChanged.AddListener(SetSFXVolume);
+        AudioManager.Instance.PlayAudioOneShot(hoverAudioSource, startMainMenuClip);
+    }
     public void PlayUiAccept(){
     AudioManager.Instance.PlayAudioOneShot(audioSource, uiAcceptClip);
 
@@ -32,12 +43,14 @@ public class MainMenuAudio : MonoBehaviour
 
     public void SetSFXVolume(float sliderValue)
     {
+        Debug.Log("setting sfx " + sliderValue);
         mainAudioMixer.SetFloat("SfxVol", Mathf.Log10(sliderValue) * 20);
     }
 
     public void SetMusicVolume(float sliderValue)
     {
-        mainAudioMixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+        Debug.Log("setting music " + sliderValue);
+        mainAudioMixer.SetFloat("MusicVol", (Mathf.Log10(sliderValue) * 20) -6);
        
     }
 }
