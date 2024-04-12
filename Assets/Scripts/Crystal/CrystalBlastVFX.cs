402e9c472d7d416c7d55201cac25338cc35b9f3e
ally <roa.ayleen07@gmail.com>
2024-04-11 22:43:06 -0400

    Squashed commit of the following:
    commit 9db6d74cc16308faa963a942553873785a86dd6e
    Author: Raaee <99899402+Raaee@users.noreply.github.com>
    Date:   Thu Apr 11 21:20:54 2024 -0400
    
        Update Overworld Game Scene.unity
    
    commit 5a2aca20600b2e2aaa2af5f697f1df1f721279b0
    Author: Raaee <99899402+Raaee@users.noreply.github.com>
    Date:   Thu Apr 11 21:19:46 2024 -0400
    
        blastvfx script fix
    
    commit 6c2a29c304427c054b292492476939c548802dce
    Merge: d1a342e 26df46f
    Author: Raeus Aranguren-Viegas <99899402+Raaee@users.noreply.github.com>
    Date:   Thu Apr 11 21:14:55 2024 -0400
    
        Merge pull request #83 from Raaee/blastVFX
    
        blast VFX complete
    
    commit 26df46f1ca683b52088df443764147928a7421e0
    Author: Raaee <99899402+Raaee@users.noreply.github.com>
    Date:   Thu Apr 11 21:14:36 2024 -0400
    
        blast VFX complete

---
+++ Assets/Scripts/Crystal/CrystalBlastVFX.cs
@@ -0,0 +1,54 @@
+using com.cyborgAssets.inspectorButtonPro;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalBlastVFX : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem blastVFX;
+    private float maxRadius = 8f;
+    private float maxDonutRadius = 0.35f;
+    private bool blastActive = false;
+
+    private void Start() {
+        DisactivateVFX();
+        var shape = blastVFX.shape;
+        shape.donutRadius = maxDonutRadius;
+    }
+    [ProButton]
+    public void ActivateVFX() {
+        BlastActive(true);
+        ResetVFX();
+        blastVFX.transform.parent.gameObject.SetActive(true);
+        StartCoroutine(BlastIncrease());
+    }
+    public void DisactivateVFX() {
+        blastVFX.transform.parent.gameObject.SetActive(false);
+    }
+    public void ResetVFX() {
+        var shape = blastVFX.shape;
+        shape.radius = 0f;
+        shape.donutRadius = 0f;
+    }
+    public IEnumerator BlastIncrease() {
+        var shape = blastVFX.shape;
+
+        while (blastActive) {
+            shape.radius += 0.1f;
+            if (shape.radius >= maxRadius) {
+                shape.radius = maxRadius;
+            }
+
+            if (shape.radius == maxRadius) {
+                blastActive = false;
+                yield return new WaitForSeconds(0.2f);
+                DisactivateVFX();
+            }
+            yield return new WaitForSeconds(0.005f);
+        }
+    }
+    public void BlastActive(bool isActive) {
+        blastActive = isActive;
+    }
+
+}
